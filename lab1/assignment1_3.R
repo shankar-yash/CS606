2e5d3330e3c25f89ed78710598d50bbd8d1e81d1
shankar-yash <86920008+shankar-yash@users.noreply.github.com>
2021-08-17 17:40:17 +0530

    Update assignment1_3.R

--- lab1/assignment1_3.R
+++ lab1/assignment1_3.R
@@ -1,4 +1,5 @@
 #write the above two programs using functions
+#WAP in r to print the highest of 3 numbers using function.
 maximum<-function(a,b,c)
 {
     max<-a;
@@ -10,6 +11,8 @@ maximum<-function(a,b,c)
 }
 maximum(20,40,30)
 
+
+# WAP in r to find and display the sum on n natural no. using function
 sum <- function(n) {
     return ((n*(n+1))/2)
 }
